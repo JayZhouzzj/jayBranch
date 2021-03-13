035310394c04d39622fd4914008f174901aa11e7
JayZhouzzj <jayzhouzzj@gmail.com>
2021-03-13 12:10:12 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-hrwe2h
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-hrwe2h)
---
+++ src/app/frontend_data.ts
@@ -0,0 +1,11 @@
+export const frontendData = {
+  path:
+    "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png",
+  orgName: "Awesome Math",
+  topBarY: 100
+};
+
+export let pageData = {
+  pageName: "The Game Name",
+  category: "Linear Algebra"
+};
